af4911ef1579ac09dbe2e9fe0ec4691ef1dc5d08
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-08 20:58:17 +0530

    Add dependencies and functionality for email verification

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,62 @@
 import express from "express";
 
-import { Request, Response } from "express";
+// email handler
+import * as nodemailer from "nodemailer";
+
+// unique string generator
+import { v4 as uuidv4 } from "uuid";
+
+// bycrypt
+import bcrypt from "bcrypt";
+
+// import dotenv
+import dotenv from "dotenv";
+dotenv.config();
+
 import { AppDataSource } from "../index";
 import { HomlyUser } from "../entities/User";
 
 const homly_user = express.Router();
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_EMAIL_PASS,
+  }
+});
+
+transporter.verify((error: any, success: any) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+// send verification email
+const sendVerificationEmail = (email: string, serviceNo: string) => {
+    const url = `http://localhost:3002/`;
+    const verificationCode = uuidv4()+serviceNo;
+    const mailOptions = {
+      from: process.env.AUTH_EMAIL,
+      to: email,
+      subject: "Homly User Verification",
+      html: `<h1>Homly User Verification</h1><p>Click the link below to verify your account: <a href="${url}verify/${serviceNo}/${verificationCode}">Verify</a></p>`,
+      // text: `Click the link below to verify your account: ${url}verify/${serviceNo}/${verificationCode}`,
+    
+      // hash the verification code
+      
+  };
+
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
 
 const userExist = async (ServiceNo: string) => {
   const usersWithSameNo = await AppDataSource.manager.findOneBy(HomlyUser, {
@@ -19,6 +71,8 @@ const userExist = async (ServiceNo: string) => {
   
 };
 
+
+
 homly_user.get("/users", async (req, res) => {
   const users = await AppDataSource.manager.find(HomlyUser);
   res.json(users);
@@ -28,13 +82,14 @@ homly_user.post("/users/add", async (req, res) => {
   const { ServiceNo, Password, Email, ContactNo, image } = req.body;
 
   if (await userExist(ServiceNo)) {
-    const user = HomlyUser.create({
-      service_number: ServiceNo,
-      password: Password,
-      email: Email,
-      contact_number: ContactNo,
-      image,
-    });
+    sendVerificationEmail(Email, ServiceNo);
+    // const user = HomlyUser.create({
+    //   service_number: ServiceNo,
+    //   password: Password,
+    //   email: Email,
+    //   contact_number: ContactNo,
+    //   image,
+    // });
     //  await user.save();
     return res.status(201).json({message: "User successfully registred",success:true});
   }else{
