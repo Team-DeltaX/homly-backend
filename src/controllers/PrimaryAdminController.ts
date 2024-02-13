1be954cc5c504cd2041c5e84a73e20dcaadcc351
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-02-14 00:14:57 +0530

    update location admin controller created(disable admin)

--- src/controllers/PrimaryAdminController.ts
+++ src/controllers/PrimaryAdminController.ts
@@ -53,6 +53,22 @@ router.get("/all", async (req: Request, res: Response) => {
   }
 });
 
+router.put("/disable/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const dis = req.body;
 
+  try {
+    await AppDataSource.manager.update(
+      LocationAdmin,
+      { AdminNo: id },
+      { Disabled: dis }
+    );
+
+    res.status(200).json({ message: "disable sucessful!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export { router };
