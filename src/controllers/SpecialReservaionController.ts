01f66e760beb25708608e5bd065d8a972316b125
Booshana Namudara <booshiwork@gmail.com>
2024-02-20 18:13:18 +0530

    update controller

--- src/controllers/SpecialReservaionController.ts
+++ src/controllers/SpecialReservaionController.ts
@@ -3,24 +3,7 @@ const router = express.Router();
 import { SpecailReservation } from "../entities/SpecialReservation";
 import { Request, Response } from "express";
 import { AppDataSource } from "../index";
-import { error } from "console";
-// var nodemailer = require('nodemailer');
-import nodemailer from "nodemailer";
 
-// var transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "mnbro123321@gmail.com",
-//     pass: "hbqa kzdq keop rxbl",
-//   },
-// });
-// transporter.verify(function (error, success) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Server is ready to take our messages");
-//   }
-// });
 
 const AddSpecialResrvation = async (req: Request, res: Response) => {
   const {
@@ -48,24 +31,7 @@ const AddSpecialResrvation = async (req: Request, res: Response) => {
       ])
       .execute();
 
-    //   var mailOptions = {
-    //     from: "mnbro123321@gmail.com",
-    //     to: Email,
-    //     subject: "You Are Added as Location Admin in Homly",
-    //     html: `<p>Your Added As Location Admin in Homly. Your <strong>Password</strong> is <strong>${Password}</strong> and Your <strong>UserName</strong> is <strong>${UserName}</strong> and Your Work Location is ${WorkLocation}</p><br><p>in any problem please contact the primary admin</p>`
-    //   };
-
-    
-
-    // transporter.sendMail(mailOptions, function (error: any, info) {
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     console.log("Email sent: " + info.response);
-    //   }
-    // });
-
-    // console.log("sucess added");
+  
 
     res.status(200).json({ message: "Special Reservation added successfully" });
 
--- src/routes/SpecialReservationRouter.ts
+++ src/routes/SpecialReservationRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { AddSpecialResrvation } from "../controllers/PrimaryAdminController";
+import { AddSpecialResrvation } from "../controllers/SpecialReservaionController";
 
 const SpecialReservationRouter = express.Router ();
 SpecialReservationRouter.post("/",AddSpecialResrvation)
