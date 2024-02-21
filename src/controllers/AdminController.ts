06714bf0c894069b03848142ba19d1b470ef3a63
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-02-21 19:27:25 +0530

    add change password functionality

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,48 +1,62 @@
 import { AppDataSource } from "../index";
 import { Request, Response } from "express";
 
-import { Admin } from "../entities/Admin";
+import { HomlyAdmin } from "../entities/HomlyAdmin";
+
+// bycrypt
+import bcrypt from "bcrypt";
 
 const adminLogin = async (req: Request, res: Response) => {
   const { adminId, password } = req.body;
   await AppDataSource.manager
-    .findOneBy(Admin, {
+    .findOneBy(HomlyAdmin, {
       AdminNo: adminId,
     })
     .then((admin) => {
       if (admin) {
-        if (admin.Password === password) {
-          if (admin.Role === "PrimaryAdmin") {
-            res
-              .status(200)
-              .json({
-                message: "Login Success",
-                success: true,
-                role: "PrimaryAdmin",
-              });
-          } else {
-            if (admin.Verified) {
+        bcrypt
+          .compare(password, admin.Password)
+          .then((result) => {
+            if (result) {
+              if (admin.Role === "PrimaryAdmin") {
+                res.status(200).json({
+                  message: "Login Success",
+                  success: true,
+                  role: "PrimaryAdmin",
+                });
+              } else {
+                if (admin.Verified) {
+                  res.status(200).json({
+                    message: "Login Success",
+                    success: true,
+                    verified: true,
+                    role: "LocationAdmin",
+                  });
+                } else {
+                  res.status(200).json({
+                    message:
+                      "Login Success,Please Change Your default password",
+                    success: true,
+                    verified: false,
+                  });
+                }
+              }
+            } else {
               res
                 .status(200)
                 .json({
-                  message: "Login Success",
-                  success: true,
-                  verified: true,
-                  role: "LocationAdmin",
+                  message: "Invalid Username or Password",
+                  success: false,
                 });
-            } else {
-              res.status(200).json({
-                message: "Login Success,Please Change Your default password",
-                success: true,
-                verified: false,
-              });
             }
-          }
-        } else {
-          res.status(200).json({ message: "Invalid Username or Password" ,success:false});
-        }
+          })
+          .catch((error) => {
+            res.status(500).json({ message: "Internal Server Error" });
+          });
       } else {
-        res.status(200).json({ message: "You are not an Admin" ,success:false});
+        res
+          .status(200)
+          .json({ message: "You are not an Admin", success: false });
       }
     })
     .catch((error) => {
@@ -50,4 +64,28 @@ const adminLogin = async (req: Request, res: Response) => {
     });
 };
 
-export { adminLogin };
+const changeDefaultPassword = async (req: Request, res: Response) => {
+  const { adminId, password } = req.body;
+  await AppDataSource.manager
+    .findOneBy(HomlyAdmin, { AdminNo: adminId })
+    .then((admin) => {
+      if (admin) {
+        const saltRound = 10;
+        bcrypt
+          .hash(password, saltRound)
+          .then((hash) => {
+            AppDataSource.manager.update(
+              HomlyAdmin,
+              { AdminNo: adminId },
+              { Password: hash, Verified: true }
+            );
+            res.status(200).json({ message: "Password Changed", success: true });
+          })
+          .catch((error) => {
+            res.status(500).json({ message: "Internal Server Error" });
+          });
+      }
+    });
+};
+
+export { adminLogin,changeDefaultPassword };
--- src/routes/AdminRouters.ts
+++ src/routes/AdminRouters.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { adminLogin } from "../controllers/AdminController";
+import { adminLogin,changeDefaultPassword } from "../controllers/AdminController";
 
 const admin_router = express.Router();
 
 admin_router.post("/", adminLogin);
+admin_router.put("/",changeDefaultPassword);
 
 export { admin_router };
