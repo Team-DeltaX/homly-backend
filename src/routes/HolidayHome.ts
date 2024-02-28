cba2af6f769be409280420846312059dac9e95a8
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-02-28 17:45:47 +0530

    Add getRoomRental endpoint to HolidayHomeRouter

--- src/routes/HolidayHome.ts
+++ src/routes/HolidayHome.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getHolidayHomes, getHolidayHomesDetails, createHolidayHome, getSelectedRooms, getRoom } from "../controllers/HolidayHomeController";
+import { getHolidayHomes, getHolidayHomesDetails, createHolidayHome, getSelectedRooms, getRoom, getRoomRental } from "../controllers/HolidayHomeController";
 
 
 const HolidayHomeRouter = express.Router();
@@ -8,6 +8,8 @@ HolidayHomeRouter.get("/", getHolidayHomes);
 HolidayHomeRouter.get("/:HolidayHomeId", getHolidayHomesDetails);
 HolidayHomeRouter.get("/:HolidayHomeId/:unitCode", getSelectedRooms);
 HolidayHomeRouter.get("/room/:HolidayHomeId/:roomCode", getRoom);
+HolidayHomeRouter.get("/rental/:HolidayHomeId/:HRUId", getRoomRental);
+
 HolidayHomeRouter.post("/", createHolidayHome);
 
 
