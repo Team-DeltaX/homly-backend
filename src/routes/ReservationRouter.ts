885ab91d9cac133b11129a674d207ef772dc487e
Booshana Namudara <booshiwork@gmail.com>
2024-03-11 15:56:29 +0530

    updated

--- src/routes/ReservationRouter.ts
+++ src/routes/ReservationRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {getReservation,AddResrvation,getHolidayHomeNames, getRooms, AddComplaint} from "../controllers/ReservationController"
+import {getOngoingReservation, getPastReservation} from "../controllers/PrimaryAdminController";
 
 const ReservationRouter = express.Router ();
 
@@ -8,5 +9,7 @@ ReservationRouter.post("/auth/reservation",AddResrvation)
 ReservationRouter.get("/reservation/holidayhomes",getHolidayHomeNames);
 ReservationRouter.get("/reservation/rooms",getRooms);
 ReservationRouter.post("/reservation/AddComplaint",AddComplaint);
+ReservationRouter.get("/reservation/ongoing",getOngoingReservation);
+ReservationRouter.get("/reservation/past",getPastReservation);
 
 export {ReservationRouter}
