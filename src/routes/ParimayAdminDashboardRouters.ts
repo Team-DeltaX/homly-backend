4d2eb488815e6c58b4194466420212e3c1c3efde
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-03-13 12:05:24 +0530

    get room income and hall income controllers created in primary admin controller

--- src/routes/ParimayAdminDashboardRouters.ts
+++ src/routes/ParimayAdminDashboardRouters.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Active_InActive_HHcount, Earning, HHcount, getBookingscounts, gethallcount } from '../controllers/PrimaryAdminController';
+import { Active_InActive_HHcount, Earning, HHcount, Hallincome, Roomincome, getBookingscounts, gethallcount, getroomcount } from '../controllers/PrimaryAdminController';
 const PrimaryAdminDashboardRouter = express.Router();
 
 
@@ -10,6 +10,11 @@ PrimaryAdminDashboardRouter.get('/auth/earning',Earning)
 PrimaryAdminDashboardRouter.get('/auth/hhstatus',Active_InActive_HHcount )
 PrimaryAdminDashboardRouter.get('/auth/bookingcount',getBookingscounts )
 PrimaryAdminDashboardRouter.get('/auth/hall',gethallcount)
+PrimaryAdminDashboardRouter.get('/auth/room',getroomcount)
+PrimaryAdminDashboardRouter.get('/auth/hallincome',Hallincome)
+PrimaryAdminDashboardRouter.get('/auth/roomincome',Roomincome)
+
+
 
 
 
