fd9873d65a49a0cc90912d6c156fe9fbd0b6984a
DinaliKaumadi <138236249+DinaliKaumadi@users.noreply.github.com>
2024-03-21 19:26:55 +0530

    update code

--- src/controllers/ReportController.ts
+++ src/controllers/ReportController.ts
@@ -4,29 +4,51 @@ import { Reservation } from "../entities/Reservation";
 import { LessThanOrEqual, MoreThanOrEqual } from "typeorm";
 
 const getGeneratedReport = async (req: Request, res: Response) => {
-  const { from, to } = req.query; // Assuming from and to dates are provided in the query parameters
+  const dates = req.query; // Assuming from and to dates are provided in the query parameters
 
-  if (!from || !to) {
-    return res
-      .status(400)
-      .json({ error: "Please provide both from and to dates" });
+  let checkinDate, checkoutDate;
+
+  if (dates.from) {
+    checkinDate = new Date(dates.from as string);
   }
-
+  if (dates.to) {
+    checkoutDate = new Date(dates.to as string);
+  }
+  // if (!from || !to) {
+  //   return res
+  //     .status(400)
+  //     .json({ error: "Please provide both from and to dates" });
+  // }
+  // console.log(dates.from);
   // Convert string dates to Date objects
-  const fromDate = new Date(from.toString());
-  const toDate = new Date(to.toString());
-
-  // Assuming AppDataSource.manager.find method supports filtering by date range
-  try {
-    const filteredReservations = await AppDataSource.manager.find(Reservation, {
-      where: {
-        CheckinDate: MoreThanOrEqual(fromDate),
-        CheckoutDate: LessThanOrEqual(toDate),
-      },
-    });
-    res.status(200).json({ fromDate, toDate });
-  } catch (err) {
-    res.status(404).json("error");
+  // if (dates.from && dates.to) {
+  //   const fromDate = new Date(dates.from as string);
+  //   const toDate = new Date(dates.to as string);
+  if (checkinDate && checkoutDate) {
+    // Assuming AppDataSource.manager.find method supports filtering by date range
+    try {
+      // const filteredReservations = await AppDataSource.manager.find(
+      //   Reservation,
+      //   {
+      //     where: {
+      //       CheckinDate: MoreThanOrEqual(new Date(fromDate)),
+      //       CheckoutDate: LessThanOrEqual(new Date(toDate)),
+      //     },
+      //   }
+      // );
+      const reservation = await AppDataSource.manager.find(Reservation, {
+        where: [
+          {
+            // HolidayHome: holidayHomeId,
+            CheckoutDate: LessThanOrEqual(new Date(checkinDate)),
+          },
+        ],
+      });
+      res.status(200).json({ reservation });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
   }
 };
 
