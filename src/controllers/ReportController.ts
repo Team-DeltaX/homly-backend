6dcc5ea822e7feceb15255e8a257fb5f2cb03a0a
DinaliKaumadi <138236249+DinaliKaumadi@users.noreply.github.com>
2024-03-21 19:03:23 +0530

    update code

--- src/controllers/ReportController.ts
+++ src/controllers/ReportController.ts
@@ -17,14 +17,17 @@ const getGeneratedReport = async (req: Request, res: Response) => {
   const toDate = new Date(to.toString());
 
   // Assuming AppDataSource.manager.find method supports filtering by date range
-  const filteredReservations = await AppDataSource.manager.find(Reservation, {
-    where: {
-      CheckinDate: MoreThanOrEqual(fromDate),
-      CheckoutDate: LessThanOrEqual(toDate),
-    },
-  });
-
-  res.json(filteredReservations);
+  try {
+    const filteredReservations = await AppDataSource.manager.find(Reservation, {
+      where: {
+        CheckinDate: MoreThanOrEqual(fromDate),
+        CheckoutDate: LessThanOrEqual(toDate),
+      },
+    });
+    res.status(200).json({ fromDate, toDate });
+  } catch (err) {
+    res.status(404).json("error");
+  }
 };
 
 export { getGeneratedReport };
