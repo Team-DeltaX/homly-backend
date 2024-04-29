fa2f836caa65f083f37c10207b99c91172f49d48
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-04-29 22:48:27 +0530

    daily run function crearted in primary admin controller

--- src/controllers/PrimaryAdminController.ts
+++ src/controllers/PrimaryAdminController.ts
@@ -27,6 +27,7 @@ import { Hall } from "../entities/Hall";
 import { Room } from "../entities/Room";
 import { ReservedRooms } from "../entities/ReservedRooms";
 import { ReservedHalls } from "../entities/ReservedHalls";
+const schedule = require('node-schedule');
 dotenv.config();
 
 var transporter = nodemailer.createTransport({
@@ -739,3 +740,7 @@ export const get_holiday_home_rating = async (req: Request, res: Response) => {
     res.status(500).json({ message: "error in getting holiday home rating" });
   }
 };
+
+export const everyFiveSeconds = schedule.scheduleJob('0 0 * * *', () => {
+  console.log('Task executed every 5 seconds:', new Date().toLocaleTimeString());
+});
