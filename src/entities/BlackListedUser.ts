d089dabf6f771bcb77e6176fe307189ecdf9955b
Booshana Namudara <booshiwork@gmail.com>
2024-06-13 16:33:43 +0530

    update

--- src/entities/BlackListedUser.ts
+++ src/entities/BlackListedUser.ts
@@ -1,7 +1,4 @@
 import { Entity, PrimaryGeneratedColumn, Column ,PrimaryColumn, OneToMany, ManyToOne, JoinColumn, OneToOne,CreateDateColumn, BaseEntity} from 'typeorm';
-
-
-
 @Entity()
 export class BlackListedUser extends BaseEntity{
   @PrimaryGeneratedColumn('uuid')
