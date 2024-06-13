2134b5f2072dace4b444f6fae7a75e812d8696c1
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-06-13 16:55:57 +0530

    Refactor image variable name in HolidayHomeController

--- src/entities/BlackListedUser.ts
+++ src/entities/BlackListedUser.ts
@@ -1,24 +1,29 @@
-import { Entity, PrimaryGeneratedColumn, Column ,PrimaryColumn, OneToMany, ManyToOne, JoinColumn, OneToOne,CreateDateColumn, BaseEntity} from 'typeorm';
-
-
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  PrimaryColumn,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
+  OneToOne,
+  CreateDateColumn,
+  BaseEntity,
+} from "typeorm";
 
 @Entity()
-export class BlackListedUser extends BaseEntity{
-    Save() {
-        throw new Error("Method not implemented.");
-    }
-  @PrimaryGeneratedColumn('uuid')
-  BlackListId!: number ;
+export class BlackListedUser extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  BlackListId!: number;
 
   @Column()
-  BlackListReason!:String;
+  BlackListReason!: String;
 
-  
   @CreateDateColumn({
     nullable: false,
   })
   Date!: Date;
 
   @Column()
-  ServiceNo!:String;
+  ServiceNo!: String;
 }
